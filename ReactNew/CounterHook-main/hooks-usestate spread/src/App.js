87134dc13e302ec96244009b6b2a978420f8449c
AraH94 <arahoedemakers@hotmail.com>
2023-04-20 13:37:44 +0200

    usestate

--- ReactNew/CounterHook-main/hooks-usestate spread/src/App.js
+++ ReactNew/CounterHook-main/hooks-usestate spread/src/App.js
@@ -1,28 +1,37 @@
 import './App.css';
 import React, { useState } from 'react';
+
 function App() {
-  const [state, setState] = useState({ count: 2, name: "Bene" });
-  const count = state.count;
-  const name = state.name;
-//spread operator is used to prevent overwriting previous info
-  function decrement() {
-    setState(prevState => { return { ...prevState, count: prevState.count - 1 } });
-  }
+	const [state, setState] = useState({ count: 8, name: "Ara" });
+	const count = state.count;
+	const name = state.name;
 
-  function increment() {
-    setState(prevState => { return { ...prevState, count: prevState.count + 1 } });
-  }
+	const [firstName, setName] = useState('');
+	const handleChange = (e) => {
+		setName(e.target.value)
+	}
 
-  return (
-    <div className="App">
-      <header className="App-header">
-        <button onClick={decrement}>-</button>
-        <span>{count}</span>
-        <button onClick={increment}>+</button>
-        <p>Name: {name}</p>
-      </header>
-    </div>
-  );
+	//spread operator is used to prevent overwriting previous info
+	function decrement() {
+		setState(prevState => { return { ...prevState, count: prevState.count - 1 } });
+	}
+
+	function increment() {
+		setState(prevState => { return { ...prevState, count: prevState.count + 1 } });
+	}
+
+	return (
+		<div className="App">
+			<header className="App-header">
+				<p>Hello {firstName}</p>
+				<input type="text" value={firstName} onChange={handleChange}></input>
+				<button onClick={decrement}>-</button>
+				<span>{count}</span>
+				<button onClick={increment}>+</button>
+				<p>Name: {name}</p>
+			</header>
+		</div>
+	);
 }
 
 export default App;
--- ReactNew/CounterHook-main/hooks-usestate/src/App.js
+++ ReactNew/CounterHook-main/hooks-usestate/src/App.js
@@ -1,28 +1,11 @@
+import React from 'react';
+import Count from './Count';
 import './App.css';
-import React, { useState } from 'react';
-
 
 function App() {
-  const [count, setCount] = useState(0)
-  // actual veranderaar begin default
-
-  function decrement() {
-    // setCount(count - 1) 
-    //inscope problem variables overwriting 
-    setCount(prevCount => prevCount - 1)
-  } function increment() {
-    setCount(prevCount => prevCount + 1)
-  }
-
   return (
-    <div className="App">
-      <header className="App-header">
-        <button onClick={decrement}>-</button>
-        <span>{count}</span>
-        <button onClick={increment}>+</button>
-      </header>
-    </div>
-  );
+	<Count/>
+  )
 }
 
 export default App;
